768a08dbb48208c82c8d7dd794ada497a2d4220a
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-02-08 01:16:02 +0900

    署名

--- BS-CameraMovement/Properties/AssemblyInfo.cs
+++ BS-CameraMovement/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BS-CameraMovement")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Copyright ©  2026 Rynan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
